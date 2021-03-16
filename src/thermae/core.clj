5507ec852971857d449f552e95df636e16322af2
Mikael Brockman <mikael@brockman.se>
2021-03-16 16:57:27 +0200

    make good

--- src/thermae/core.clj
+++ src/thermae/core.clj
@@ -3,7 +3,7 @@
   (:use hiccup.core)
   (:use hiccup.page)
   (:use slugger.core)
-  (:use [clojure.string :only (blank?)])
+  (:use [clojure.string :only (blank? starts-with? replace)])
   (:use [clojure.pprint :only (pprint)])
   (:require
    [datascript.core :as d]
@@ -36,17 +36,32 @@
                      :find (pull ?e [:node/title
                                      :block/string
                                      :block/children {:block/children ...}])
-                     :where [?e :node/title "books/Prayer"]]
+                     :where [?e :node/title "books/Focusing"]]
                    db)]
     x))
 
+(def foo
+  '[
+    :find (pull ?y [*])
+    :where
+    [?y :block/page ?page]
+    (not [?y :block/heading])
+    [?x :block/page ?page]
+    [?x :block/refs ?attr]
+    [?x :block/refs ?value]
+    [?attr :node/title "Category"]
+    [?value :node/title "books"]
+    ])
+
 (def markup-parser
   (insta/parser "
-    root = attr / thing
+    root = attr / blockquote / thing
+
+    blockquote = <'> '> thing+
 
     <thing> = (alias | xlink | link | img | text)+
 
-    attr = #'[^:]+' '::' thing+
+    attr = #'[^:]+' <'::'> thing+
 
     alias = <'['> text <']'> <'((('> uid <')))'>
     xlink = <'['> text <']'> <'('> #'[^)]+' <')'>
@@ -57,27 +72,50 @@
     uid = #'[a-zA-Z0-9]+'
   "))
 
+(markup-parser "[hey ho wow](http://google.com)")
+
 (defn tree-to-html [tree]
   (let [rules
-        {:root (fn [& x] (vec (concat [:div] x)))
+        {
          :text str
-         :link (fn [x] [:a {:href "#" :class "link"} x])
-         :img (fn [x] [:img {:src x}])
+
+         :root
+         (fn [& x] (vec (concat [:div] x)))
+
+         :link
+         (fn [& x] [:a {:href "#" :class "ref"} x])
+
+         :xlink
+         (fn [x href] [:a {:href href} x])
+
+         :img
+         (fn [x] [:img {:src x}])
+
+         :attr
+         (fn [attr & xs] `[:span [:b ~attr] ": " ~@xs])
          }]
     (insta/transform rules tree)))
 
-(defn render-block [x]
+(defn process-body [x]
+  (-> x
+   (replace #" \(\[Location.*?\]\(.*?\)\)" "")
+   (replace #"Highlights first synced by #Readwise (.*)" "$1 highlights:")
+   (replace #"New highlights added (.*?) at .*" "$1 highlights:")))
+
+(defn render-block [x indent]
   (let [body (x :block/string)]
-    [:div
+    [:div {:class (if (starts-with? body "**Tags**") "hide" nil)
+           :style (str "margin-left: " indent "em")}
      (vec (concat
            (if (not (blank? body))
-             [:p (let [tree (markup-parser body)]
+             [:p (let [tree (markup-parser (process-body body))]
                    (if (insta/failure? tree)
                      (throw (ex-info "parse error" tree))
                      (tree-to-html
                       tree)))]
              [])
-           (mapv render-block (x :block/children))))]))
+           (mapv #(render-block % (inc indent))
+                 (x :block/children))))]))
 
 ;; (def all-pages-1
 ;;   (into {}
@@ -88,7 +126,7 @@
 ;;             [:h1 (node :node/title)]
 ;;             (map render-block (node :block/children))]])))
 
-(defn preify [x] [:pre (with-out-str (pprint (render-block x)))])
+(defn preify [x] [:pre (with-out-str (pprint (render-block x 0)))])
 
 (def all-pages
   (into {}
@@ -99,7 +137,7 @@
               [:meta {:charset "utf-8"}]
               [:link {:rel "stylesheet" :href "index.css"}]
               [:h1 (node :node/title)]
-              (map render-block (node :block/children))
+              (map #(render-block % 0) (node :block/children))
               ))])))
 
 (defn pages []
